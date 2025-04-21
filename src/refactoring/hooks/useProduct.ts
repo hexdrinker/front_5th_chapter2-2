f043c54135756e287c5becead4481474344fcac5
Park YuongHo <hexdrinker2020@gmail.com>
2025-04-21 22:56:17 +0900

    feat: useProduct hook 코드 구현

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  const addProduct = (product: Product) => {
+    setProducts([...products, product]);
+  };
+
+  const updateProduct = (product: Product) => {
+    setProducts(products.map((p) => (p.id === product.id ? product : p)));
+  };
+
+  useEffect(() => {
+    setProducts(initialProducts);
+  }, [initialProducts]);
+
   return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products,
+    updateProduct,
+    addProduct,
   };
 };
