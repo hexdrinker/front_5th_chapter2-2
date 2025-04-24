e32a9ae8de0b3e2833f4dab5eb6cdfb2719e9645
Park YuongHo <hexdrinker2020@gmail.com>
2025-04-25 01:02:43 +0900

    feat: gh-pages 패키지 설치 및 배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default mergeConfig(
   defineConfig({
+    base: "./",
     plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: {
+          refactoring: new URL("./index.refactoring.html", import.meta.url)
+            .pathname,
+        },
+      },
+    },
   }),
   defineTestConfig({
     test: {
       globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts",
     },
-  })
-)
+  }),
+);
